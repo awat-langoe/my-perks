989488490bf4df514c33b9f5fa77acbe0653153d
Awat Abdollahzadeh Langø <awat.langoe@gmail.com>
2024-09-14 12:06:31 +0200

    extract perk card to its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatIconModule } from "@angular/material/icon";
 import { RouterOutlet } from "@angular/router";
-import { CustomThemeDirective } from "./directive/custom-theme.directive";
+import { PerkCardComponent } from "./component/perk-card/perk-card.component";
 import { PerksService } from "./service/perks/perks.service";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    CustomThemeDirective,
-  ],
+  imports: [CommonModule, RouterOutlet, PerkCardComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
---
+++ src/app/component/perk-card/perk-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { CustomThemeDirective } from "../../directive/custom-theme.directive";
+import { Perk } from "../../service/perks/model/perk.model";
+
+@Component({
+  selector: "app-perk-card",
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    CustomThemeDirective,
+  ],
+  templateUrl: "./perk-card.component.html",
+  styleUrl: "./perk-card.component.scss",
+})
+export class PerkCardComponent {
+  @Input({ required: true }) perk!: Perk;
+}
