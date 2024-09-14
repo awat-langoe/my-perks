a8c8f397eb032f60863bc9b8eec143ee8042424b
Awat Abdollahzadeh Langø <awat.langoe@gmail.com>
2024-09-14 14:41:50 +0200

    add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { RouterOutlet } from "@angular/router";
 import { PerkCardComponent } from "./component/perk-card/perk-card.component";
 import { PerksService } from "./service/perks/perks.service";
@@ -7,7 +8,7 @@ import { PerksService } from "./service/perks/perks.service";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PerkCardComponent],
+  imports: [CommonModule, RouterOutlet, PerkCardComponent, MatToolbarModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
